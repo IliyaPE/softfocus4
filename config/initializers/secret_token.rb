b256eb08693b8086f27f6e4375d68c108ac374ae
Adrien Jarthon <jobs@adrienjarthon.com>
2012-02-21 16:17:11 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Softfocus::Application.config.secret_token = '7496b767cc9a9380f086205ad3c2c0b78f355e6bdf393551e06397c7f244be4c945826b03801de27414ae5f1fb3b173cf95f24c418361d94ecdebf7b4d126a17'
