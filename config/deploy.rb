efb11d405fa9eeaac10487a4eab3223adc2ec103
Adrien Jarthon <jobs@adrienjarthon.com>
2012-04-09 13:40:27 +0200

    deploy ready

---
+++ config/deploy.rb
@@ -0,0 +1,38 @@
+require 'capistrano_colors'
+require 'capistrano-file_db'
+require 'bundler/capistrano'
+
+set :application, "Softfocus.me website"
+
+# Repository
+set :scm, :git
+set :repository,  "git@rootbox.fr:softfocus"
+
+# Server
+server "lambda.rootbox.fr", :app, :web, :db, :primary => true
+set :user, :deploy
+set :deploy_to, "/home/deploy/softfocus"
+set :use_sudo, false
+
+# rbenv
+set :default_environment, {
+  'PATH' => "/home/deploy/.rbenv/shims:/home/deploy/.rbenv/bin:$PATH"
+}
+
+# bundler
+set :bundle_flags, "--deployment --quiet --binstubs --shebang ruby-local-exec"
+
+# unicorn commands
+namespace :deploy do
+  task :start, :roles => :app, :except => { :no_release => true } do
+    run 'sudo start softfocus'
+  end
+
+  task :stop, :roles => :app, :except => { :no_release => true } do
+    run 'sudo stop softfocus'
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run 'sudo restart softfocus'
+  end
+end
