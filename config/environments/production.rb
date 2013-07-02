0e57655c433dc77f6581bc4c1a76db073378d2c6
Adrien Jarthon <adrien.jarthon@dimelo.com>
2013-07-02 11:45:31 +0200

    more debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Softfocus::Application.configure do
   }
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Prepend all log lines with the following tags
   # config.log_tags = [ :subdomain, :uuid ]
